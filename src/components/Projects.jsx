f11fc590b53a52dc297ae27965f6c95f54d0f22a
Yasir <yasir.akbar86@hotmail.com>
2025-11-21 21:05:16 +0000

    projects added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Carousel } from "./ui/carousel";
+import { ProjectsItems } from "../constant/data";
 
 const Projects = () => {
-  return <section>
-    <div className='container'>
+  return (
+    <section>
+      <div className="container">
         {/* Title */}
         <h2>Recent Projects</h2>
-
         {/* Card Wrapper */}
-        <div>
-            
+        <div className="relative overflow-hidden w-full h-full py-20">
+          <Carousel slides={ProjectsItems} />
         </div>
-    </div>
-  </section>
-}
+      </div>
+    </section>
+  );
+};
 
-export default Projects
+export default Projects;
