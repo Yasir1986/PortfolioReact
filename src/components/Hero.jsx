118825cd2c0beaf3171e0e5f750f0b4ecf9b8f58
Yasir1986 <yasir.akbar86@hotmail.com>
2026-03-04 23:04:00 +0000

    small fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -47,7 +47,7 @@ const Hero = () => {
             className="max-w-[820px] mx-auto mt-4 mb-6"
           >
             Hi, I’m Yasir Akbar Senior Frontend Developer / Software Engineer
-            With 7+ years of experience, I build seamless, high-performance
+            With 10+ years of experience, I build seamless, high-performance
             interfaces that blend elegant design with strong engineering
             principles. I excel at turning complex business requirements into
             scalable, user-focused applications using modern frontend ecosystems
